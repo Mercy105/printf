3300ffde418deb7d3c875559590f0a472e5f5012
Mercy105 <wawerumercy105@gmail.com>
2023-07-22 02:46:10 -0700

    main.h

--- main.h
+++ main.h
@@ -1,30 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdarg.h>
-#include <stdio.h>
-#include <unistd.h>
-
-#define UNSED(x) (void)(x)
-#define BUFF_SIZE 1024
-/*FLAGS*/
-#define F_MINUS 1
-#define F_PLUS 2
-#define F_ZERO 4
-#define F_HASH 8
-#define F_SPACE 16
-
-/*SIZES*/
-#define S_LONG 2
-#define S_SHORT 1
-/**
- * struct fmt - Struct op
- * by donnelly and mercy
- * @fmt: The format
- * @fn: The functio associated
- */
-struct fmt
-{
-	char fat;
-	int (*fn)(va_list, char[], int, int, int, int);
-}
-
+#define NULL ((void *)0)
+int _putchar(char c);
+int _print_str(char *string);
+int _print_int(long int var);
+int _printf(const char *format, ...);
+int to_Binary(unsigned int n);
+int to_Octal(unsigned int n);
+int _power_recursion(int x, int y);
+int to_Hexa(unsigned int num);
+#endif
