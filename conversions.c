0098dedf5eb9f730853d3f2d5576aaac19734f9b
Mercy105 <wawerumercy105@gmail.com>
2023-07-25 03:26:21 -0700

    conversions.c

--- conversions.c
+++ conversions.c
@@ -1,4 +1,9 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <stdarg.h>
+#include <unistd.h>
 /**
  * to_Binary - convert unsigned int to binary.
  * @n: number passed for convertion.
