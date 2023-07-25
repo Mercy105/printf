1d3b74c67e288191403a2a51e064b0294d2c4315
Mercy105 <wawerumercy105@gmail.com>
2023-07-25 03:37:14 -0700

    print_functions.c

--- print_functions.c
+++ print_functions.c
@@ -1,5 +1,9 @@
 #include "main.h"
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <stdarg.h>
+#include <unistd.h>
 /*
  * _print_str - prints a string
  * @string: string to be printed
