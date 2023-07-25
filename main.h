b55c0b225047c88e7f6dc229ccfb99e8edcee41b
Mercy105 <wawerumercy105@gmail.com>
2023-07-25 01:46:32 -0700

    main.h

--- main.h
+++ main.h
@@ -1,20 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
-<<<<<<< HEAD
-#define NULL ((void *)0)
-=====
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
 /**
  * struct specifiers - specifiers the format
  * @specifiers: struct format
  * @f: function used
  */
+
 typedef struct specifiers
 {
 	char specifiers;
->>>>>>> 68a09e9ff992045f6a2e4116c3eb75ecdc1d1f3b
-
 	int (*f)(va_list);
 } specifiers_t;
+
 /* prototypes */
 int _printf(const char *format, ...);
 int get_function(char s, va_list args);
